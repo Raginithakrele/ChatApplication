20dcb5a1a25e0c6aecdad76f737970f468aae18f
Raginithakrele <119073703+Raginithakrele@users.noreply.github.com>
2023-09-10 14:33:20 +0530

    Add files via upload

---
+++ io.js
@@ -0,0 +1,10 @@
+const { server } = require("./server");
+
+const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+        // credentials: true,
+    },
+});
+exports.io = io;
